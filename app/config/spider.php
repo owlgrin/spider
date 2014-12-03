d620b76191aea901d617286c575635f731d012cc
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-12-03 18:06:55 +0530

    bolded the url

--- app/config/spider.php
+++ app/config/spider.php
@@ -10,7 +10,7 @@ It was so great to meet you at Unpluggd 2014. Remember me?<br/><br/>
 Thought of saying a simple hello today. Hello!<br/><br/>
 
 Mohit<br/>
-Founder & CEO</br>
-Horntell
+Founder & CEO<br/>
+<a href=\"http://horntell.com\"><strong>Horntell</strong></a>
 "
 );
